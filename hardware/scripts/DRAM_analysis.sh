8bca20fff44a9eea787d43db0a6f2996db7c17c1
swalia <swalia@AD.UCSD.EDU@peregrine.ucsd.edu>
2023-11-17 23:04:48 -0800

    update

--- hardware/scripts/DRAM_analysis.sh
+++ hardware/scripts/DRAM_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scl enable devtoolset-11 -- bash
+#scl enable devtoolset-11 -- bash
 
 CURR_DIR=$PWD
 RAMULATOR=/ramulator/ramulator
@@ -8,9 +8,9 @@ DRAMPower=/DRAMPower/build/bin/drampower_cli
 
 traces=""
 traces+="10k "
-traces+="20k "
-traces+="50k "
-traces+="100k "
+#traces+="20k "
+#traces+="50k "
+#traces+="100k "
 
 TRACES_DIR=/ramulator
 
@@ -18,17 +18,20 @@ OUT_FILE=/DRAMPower/temp
 
 parser()
 {
-    in=/dev/stdin
-    text=$1
-    out=$(grep "$text" $in | awk -F'' '{print $0}')
-    echo $out
+    in=$1
+    text=$2
+    out=$(grep "$text" $in | awk -F':' "{print $2}")
+    echo "$out"
 }
 
 
 for trace in $traces;
-do 
+do
     $RAMULATOR $TRACES_DIR/configs/DDR4-config.cfg --mode=dram  --stats $TRACES_DIR/out_${trace} $TRACES_DIR/memory_${trace}.trace
     $DRAMPower $CURR_DIR/cmd-trace-chan-0-rank-0.cmdtrace /DRAMPower/tests/tests_drampower/resources/ddr4.json > $OUT_FILE
-    echo "$(cat $OUT_FILE | parser "TOTAL ENERGY") nJ"
-    echo $(cat $TRACES_DIR/out_${trace} | parser "ramulator.ramulator_active_cycles")
+    out=$(grep "TOTAL ENERGY" $OUT_FILE | awk -F':' "{print $2}")
+    echo "$out nJ"
+    out=$(grep "ramulator.ramulator_active_cycles" $TRACES_DIR/out_${trace} | awk -F' ' "{print $2}")
+    echo $out   
 done
+
